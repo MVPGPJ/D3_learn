2f35bbf95150c796ae99092bbbee23bfe3c67053
gongpengji <292847236@qq.com>
2019-11-16 11:50:36 +0800

    gongpengji

---
+++ echarts/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div>
+      <Echarts></Echarts>
+    </div>
+    
+      <Echarts1></Echarts1>
+    
+  </div>
+</template>
+
+<script>
+import Echarts from './components/Echarts'
+import Echarts1 from './components/Echarts1'
+
+export default {
+  name: 'app',
+  components: {
+    Echarts,
+    Echarts1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ echarts/src/components/Echarts.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="myChart" :style="{width: '4 00px', height: '400px'}"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById("myChart"));
+      // 绘制图表
+      myChart.setOption({
+        title: { text: "只会笑的提莫echarts-1" },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [50, 200, 360, 100, 100, 200]
+          }
+        ]
+      });
+    }
+  }
+};
+</script>
